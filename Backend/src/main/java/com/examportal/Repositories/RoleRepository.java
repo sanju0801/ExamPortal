6150492d7ed83bfc7c330f10ab584940e22150cc
sanju <sanjuvaishnav0801@gmail.com>
2025-06-20 10:17:56 +0530

    update

--- Backend/src/main/java/com/examportal/Repositories/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.examportal.Repositories;
-
-import com.examportal.Entities.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Integer> {
-
-}
